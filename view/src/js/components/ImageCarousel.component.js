eac95e8c426c7c5ec482efd1714a0ef25ff8dfd2
Nick Steinly <njs40@duke.edu>
2023-08-01 14:29:27 -0400

    got slider to appear by adding slider css files

--- view/src/js/components/ImageCarousel.component.js
+++ view/src/js/components/ImageCarousel.component.js
@@ -1,50 +1,22 @@
-import React, { useRef } from 'react';
-import '../../css/image-carousel.css'
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
 
 export const ImageCarousel = ({ imagesData }) => {
-  const carouselRef = useRef(null);
-
-  const handleNext = () => {
-    if (carouselRef.current) {
-      carouselRef.current.scrollBy({
-        top: 0,
-        left: carouselRef.current.offsetWidth,
-        behavior: 'smooth',
-      });
-    }
-  };
-
-  const handlePrev = () => {
-    if (carouselRef.current) {
-      carouselRef.current.scrollBy({
-        top: 0,
-        left: -carouselRef.current.offsetWidth,
-        behavior: 'smooth',
-      });
-    }
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
   };
 
   return (
-    <div className="carousel-container">
-      <div className="carousel" ref={carouselRef}>
-        {imagesData &&
-          imagesData.map((imageData, index) => (
-            <div key={index} className="carousel-item">
-              <img
-                src={`data:image/png;base64,${imageData}`}
-                alt="Your Image"
-                width={'50%'}
-                height={'50%'}
-              />
-            </div>
-          ))}
-      </div>
-      <button className="prev-button" onClick={handlePrev}>
-        previous
-      </button>
-      <button className="next-button" onClick={handleNext}>
-        next
-      </button>
-    </div>
+    <Slider {...settings}>
+      {imagesData &&
+        imagesData.map((imageData, index) => (
+          <img src={`data:image/png;base64,${imageData}`} alt="Your Image" width={'50%'} height={'50%'} key={index}/>
+        ))}
+    </Slider>
   );
 };
